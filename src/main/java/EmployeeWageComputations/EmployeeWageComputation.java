8c26f4a4a7b5f8fdfe56e5bd1896e9b219aae288
adityagoel28 <adityagoel2002@gmail.com>
2023-12-19 21:27:58 +0530

    Created Employee Wage Calculation Program

--- src/main/java/EmployeeWageComputations/EmployeeWageComputation.java
+++ src/main/java/EmployeeWageComputations/EmployeeWageComputation.java
@@ -1,30 +1,60 @@
 package EmployeeWageComputations;
 
-public class EmployeeWageComputation {
+import java.util.Random;
 
-    final int WAGE_PER_HOUR = 20;
-    final int FULL_DAY_HOUR = 8;
+class EmployeeWageComputation {
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOUR = 8;
+    static final int PART_TIME_HOUR = 4;
+    static final int MAX_WORKING_HOURS = 100;
+    static final int MAX_WORKING_DAYS = 20;
 
-    // UC1: Check Attendance
-    public void checkAttendance() {
-        int attendance = (int) (Math.random() * 2);
-        if (attendance == 1) {
-            System.out.println("Employee is Present");
-        } else {
-            System.out.println("Employee is Absent");
+    enum Status {
+        FULL_TIME, ABSENT, PART_TIME
+    }
+
+    public static Status getAttendance() {
+        int chance = new Random().nextInt(3); // Generates a random number between 0 and 2
+        return chance == 0 ? Status.ABSENT : (chance == 1 ? Status.FULL_TIME : Status.PART_TIME);
+    }
+
+    public static int computeDailyWage(Status attendanceStatus) {
+        switch (attendanceStatus) {
+            case FULL_TIME:
+                return WAGE_PER_HOUR * FULL_DAY_HOUR;
+            case PART_TIME:
+                return WAGE_PER_HOUR * PART_TIME_HOUR;
+            default:
+                return 0;
         }
     }
 
-    // UC2: Calculate Daily Wage
-    public void calculateDailyWage() {
-        int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-        System.out.println("Daily Wage: " + dailyWage);
+    public static int computeMonthlyWage() {
+        int totalWage = 0;
+        int totalWorkingHours = 0;
+        int workingDays = 0;
+
+        while (workingDays < MAX_WORKING_DAYS && totalWorkingHours < MAX_WORKING_HOURS) {
+            Status attendanceStatus = getAttendance();
+            int dailyWage = computeDailyWage(attendanceStatus);
+            totalWage += dailyWage;
+            totalWorkingHours += (attendanceStatus == Status.FULL_TIME) ? FULL_DAY_HOUR : (attendanceStatus == Status.PART_TIME ? PART_TIME_HOUR : 0);
+
+            // Output for each day
+            System.out.println("Day " + (workingDays + 1) + ": " + attendanceStatus + " - Wage Earned: " + dailyWage);
+            workingDays++;
+
+            if (totalWorkingHours >= MAX_WORKING_HOURS) {
+                break;
+            }
+        }
+
+        return totalWage;
     }
 
     public static void main(String[] args) {
-        EmployeeWageComputation ewc = new EmployeeWageComputation();
-
-        ewc.checkAttendance();
-        ewc.calculateDailyWage();
+        System.out.println("Welcome to Employee Wage Computation Program");
+        int totalWage = computeMonthlyWage();
+        System.out.println("Total Monthly Wage: " + totalWage);
     }
 }
