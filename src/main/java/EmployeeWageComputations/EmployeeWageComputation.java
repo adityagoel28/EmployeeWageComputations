ed56377545c2af2d7bd95e7b2f6cfadb3bb98502
adityagoel28 <adityagoel2002@gmail.com>
2023-12-19 21:36:43 +0530

    Checking is employee is present or absent

--- src/main/java/EmployeeWageComputations/EmployeeWageComputation.java
+++ src/main/java/EmployeeWageComputations/EmployeeWageComputation.java
@@ -1,20 +1,20 @@
 package EmployeeWageComputations;
 
+import java.util.Random;
+
 public class EmployeeWageComputation {
 
     // UC1: Check Attendance
-    public void checkAttendance() {
-        int attendance = (int) (Math.random() * 2);
-        if (attendance == 1) {
-            System.out.println("Employee is Present");
-        } else {
-            System.out.println("Employee is Absent");
-        }
+    public static int getAttendance() {
+        return new Random().nextInt(2);
     }
 
     public static void main(String[] args) {
-        EmployeeWageComputation ewc = new EmployeeWageComputation();
-
-        ewc.checkAttendance();
+        int is_present = getAttendance();
+        if (is_present == 1) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
     }
 }
