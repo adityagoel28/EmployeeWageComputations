f12dc3fe260cf58aa6d83874a3d5b4ae4943b89a
adityagoel28 <adityagoel2002@gmail.com>
2023-12-19 20:53:51 +0530

    Created Employee Wage Computation Program

---
+++ src/main/java/EmployeeWageComputations/Main.java
@@ -0,0 +1,7 @@
+package EmployeeWageComputations;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
