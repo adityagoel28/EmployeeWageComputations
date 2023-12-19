29bcbc1561a5d4e4af101a2223fb7a5e125f2e70
adityagoel28 <adityagoel2002@gmail.com>
2023-12-19 21:43:35 +0530

    Added Part Time Wage also

--- src/main/java/EmployeeWageComputations/EmployeeWageComputation.java
+++ src/main/java/EmployeeWageComputations/EmployeeWageComputation.java
@@ -6,8 +6,6 @@ class EmployeeWageComputation {
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
     static final int PART_TIME_HOUR = 4;
-    static final int MAX_WORKING_HOURS = 100;
-    static final int MAX_WORKING_DAYS = 20;
 
     enum Status {
         FULL_TIME, ABSENT, PART_TIME
@@ -29,32 +27,8 @@ class EmployeeWageComputation {
         }
     }
 
-    public static int computeMonthlyWage() {
-        int totalWage = 0;
-        int totalWorkingHours = 0;
-        int workingDays = 0;
-
-        while (workingDays < MAX_WORKING_DAYS && totalWorkingHours < MAX_WORKING_HOURS) {
-            Status attendanceStatus = getAttendance();
-            int dailyWage = computeDailyWage(attendanceStatus);
-            totalWage += dailyWage;
-            totalWorkingHours += (attendanceStatus == Status.FULL_TIME) ? FULL_DAY_HOUR : (attendanceStatus == Status.PART_TIME ? PART_TIME_HOUR : 0);
-
-            // Output for each day
-            System.out.println("Day " + (workingDays + 1) + ": " + attendanceStatus + " - Wage Earned: " + dailyWage);
-            workingDays++;
-
-            if (totalWorkingHours >= MAX_WORKING_HOURS) {
-                break;
-            }
-        }
-
-        return totalWage;
-    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        int totalWage = computeMonthlyWage();
-        System.out.println("Total Monthly Wage: " + totalWage);
     }
 }
