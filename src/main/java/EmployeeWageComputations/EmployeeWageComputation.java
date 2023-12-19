46b59a547a4e8a9ea369bd1cb240d51f80afecac
adityagoel28 <adityagoel2002@gmail.com>
2023-12-19 21:15:15 +0530

    Calculated Daily wage for the employee

--- src/main/java/EmployeeWageComputations/EmployeeWageComputation.java
+++ src/main/java/EmployeeWageComputations/EmployeeWageComputation.java
@@ -2,6 +2,9 @@ package EmployeeWageComputations;
 
 public class EmployeeWageComputation {
 
+    final int WAGE_PER_HOUR = 20;
+    final int FULL_DAY_HOUR = 8;
+
     // UC1: Check Attendance
     public void checkAttendance() {
         int attendance = (int) (Math.random() * 2);
@@ -12,9 +15,16 @@ public class EmployeeWageComputation {
         }
     }
 
+    // UC2: Calculate Daily Wage
+    public void calculateDailyWage() {
+        int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        System.out.println("Daily Wage: " + dailyWage);
+    }
+
     public static void main(String[] args) {
         EmployeeWageComputation ewc = new EmployeeWageComputation();
 
         ewc.checkAttendance();
+        ewc.calculateDailyWage();
     }
 }
